43ef9086b866e5ae38df1e88a7cceea5c4999e48
Nikki Kerns <nkernns@admorgan.com>
2020-01-07 10:24:58 -0500

    all apis created.  Now add react.

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -9,21 +9,83 @@ namespace UniverseExplorer.Controllers
   [Route("api/[controller]")]
   public class PersonController : ControllerBase
   {
-    [HttpPost]
-    public ActionResult CreatePerson(int id)
+   
+       [HttpGet]
+    public ActionResult GetAllPeople()
+    {
+      // return a list of all students ordered by fullname
+      var db = new DatabaseContext();
+      return Ok(db.Persons.OrderBy(person => person.CharacterName));
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult GetOnePerson(int id)
     {
       var db = new DatabaseContext();
-      var onePlace = db.Places
-        .FirstOrDefault(i => i.Id == id);
-      if (onePlace == null)
+      var onePerson = db.Persons.FirstOrDefault(f => f.Id == id);
+      if (onePerson == null)
       {
         return NotFound();
       }
       else
       {
-
-        return Ok(onePlace);
+        return Ok(onePerson);
       }
     }
+
+    
+   
+      [HttpPost]
+    public ActionResult CreatePerson(Person person)
+    {
+      var db = new DatabaseContext();
+      person.Id = 0;
+      db.Persons.Add(person);
+      db.SaveChanges();
+      return Ok(person);
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult UpdatePerson(int id, Person person)
+    {
+      var db = new DatabaseContext();
+      var prevPerson = db.Persons.FirstOrDefault(f => f.Id == person.Id);
+      if (prevPerson == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        prevPerson.CharacterName = person.CharacterName;
+        prevPerson.ActorName = person.ActorName;
+        prevPerson.MainCharacter = person.MainCharacter;
+        prevPerson.Human = person.Human;
+
+        db.SaveChanges();
+        return Ok(prevPerson);
+      }
+    }
+
+
+
+     [HttpDelete("{id}")]
+    public ActionResult DeletePerson(int id)
+    {
+      var db = new DatabaseContext();
+      var person = db.Persons.FirstOrDefault(f => f.Id == id);
+      if (person == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        db.Persons.Remove(person);
+        db.SaveChanges();
+        return Ok();
+      }
+    }
+
+
+
   }
 }
